161a5cf6909cc3375cfd7feb93dc50761ca6856f
mjtedder <mjtedder@gmail.com>
2018-06-20 19:10:49 -0400

    orm created, functions complete, start models

--- config/orm.js
+++ config/orm.js
@@ -1,2 +1,37 @@
 // Import MySQL connection.
 let connection = require('../config/connection.js');
+
+// Object for all SQL statement functions to be used
+let orm = {
+
+    //selectAll
+    selectAll: (cb) => {
+        let queryString = 'SELECT * FROM burgers;'
+        connection.query(queryString, (err, result) => {
+            if(err) { throw err;
+            }
+            cb(result);
+        });
+    },
+    //insertOne
+    insertOne: (cb) => {
+        let queryString = 'INSERT INTO burgers (burger_name) VALUE (?);'
+        connection.query(queryString, burger_name, (err, result) => {
+            if(err) { throw err;
+            }
+            cb(result);
+        });
+    },
+    //updateOne
+    updateOne: (id, cb) => {
+        let queryString = 'UPDATE burgers SET ? WHERE ?;'
+        connection.query(queryString, {id: id}, (err, result) => {
+            if(err) { throw err;
+            }
+            cb(result);
+        })
+    }
+}
+
+// Export orm object
+module.exports = orm;
