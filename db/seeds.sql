abfc350fa5c97514dfcf734a50d18487e2620a2c
mjtedder <mjtedder@gmail.com>
2018-06-22 11:01:45 -0400

    some css added, todo: readme, deploy to heroku, more css

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,3 @@
-INSERT INTO burgers (burger_name, devoured) VALUES ('Big Sky Burger', false);
-INSERT INTO burgers (burger_name, devoured) VALUES ('BBQ Bacon Cheddar Burger', false);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Big Kahuna Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Big Sky Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('BBQ Bacon Cheddar Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Big Kahuna Burger', false);
