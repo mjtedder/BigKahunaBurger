b0d250f4cb7ed9d062ca8033501275a518948ea5
mjtedder <mjtedder@gmail.com>
2018-06-23 21:30:56 -0400

    deploy with jawsDB

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,20 @@
 //Set up MySQL connection
 let mysql = require('mysql');
 
-let connection = mysql.createConnection({
-    host: 'localhost',
-    port: 3306,
-    user: 'root',
-    password: process.env.password,
-    database: 'burgers_db'
-});
+let connection;
+if(process.env.JAWSDB_URL) {
+  //Heroku deployment
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  //local host
+    connection = mysql.createConnection({
+        root: 3000,
+        host: "localhost",
+        user: "root",
+        password: process.env.password,
+        database: "burgers_db",
+    });
+};
 
 //Connecting to DB
 connection.connect((err) => {
